3c0af1b0089d1936e6702be8ea32d7053e8cf1a2
Perry Fustero <pjfust@gmail.com>
2017-07-24 09:17:15 -0400

    finish fundamentals

--- fundamentals.rb
+++ fundamentals.rb
@@ -1,13 +1,13 @@
+# require "pry"
 # Question 1
 # Define a method called `offer_rose`, which should take one argument named `person` (String).
 # When called the method should print to the terminal:
 # "Would you take this rose, `person`, in exchange for giving an old beggar woman shelter from the bitter cold?"
 # Type your solution directly below this line:
 
-
-
-
-
+def offer_rose(person)
+  print "Would you take this rose, #{person}, in exchange for giving an old begger woman shelter from the bitter cold?"
+end
 
 # Question 2
 # Assume the following hash...
@@ -26,10 +26,8 @@ town = {
 # - Add "Belle" to the `guests` array
 # Type your solution directly below this line:
 
-
-
-
-
+town[:residents][1] = nil
+town[:castle][:guests].push("Belle")
 
 
 # Question 3
@@ -43,3 +41,11 @@ friends = ["Chip Potts", "Cogsworth", "Lumière", "Mrs. Potts"]
 # "Belle is friends with Lumière"
 # "Belle is friends with Mrs. Potts"
 # Type your solution directly below this line:
+
+
+for i in friends
+  puts "Bell is friends with #{i}"
+end
+
+
+binding.pry
