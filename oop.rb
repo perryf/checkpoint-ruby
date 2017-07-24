dd575b91e846858a28e201caffdde8c10e2f74ca
Perry Fustero <pjfust@gmail.com>
2017-07-24 09:34:35 -0400

    finish oop

--- oop.rb
+++ oop.rb
@@ -5,14 +5,23 @@
 # - The ability to "get" and "set" `name`
 # Type your solution directly below this line:
 
+class Animal
+  attr_accessor :name
+  def initialize(name)
+    @name = name
+  end
 
+  def greet
+    "Hi I'm #{@name}"
+  end
+end
 
 
 # Question 7
 # Create a new `Animal` instance with the name "Pumba".
 # Type your solution directly below this line:
 
-
+pumba = Animal.new("Pumba")
 
 
 # Question 8
@@ -23,10 +32,21 @@
 # - Only set the `king` attribute to `true` if the instance's `name` is "Simba"
 # Type your solution directly below this line:
 
-
+class Lion < Animal
+  def initialize(name)
+    @name = name
+    if @name == "Simba"
+      @king = true
+    else
+      @king = false
+    end
+  end
+end
 
 
 
 # Question 9
 # Create a new instance of `Lion` with the name "Simba".
 # Type your solution directly below this line:
+
+simba = Lion.new("Simba")
